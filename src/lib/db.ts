38f989d5fed7f135635671f71f43edc237d63104
郑焕宇 <zhenghuanyu@eslab.cn>
2025-11-20 16:59:08 +0800

    chore: add prisma and better-auth

---
+++ src/lib/db.ts
@@ -0,0 +1,21 @@
+import { env } from '@/env';
+import { PrismaClient } from '@/prisma/generated/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+const adapter = new PrismaPg({
+  connectionString: env.DATABASE_URL,
+});
+
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+
+if (env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+export default prisma;
