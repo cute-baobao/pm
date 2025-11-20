594ff485233e7ec6cfa1ece796c790c4f2a45f5b
郑焕宇 <zhenghuanyu@eslab.cn>
2025-11-20 11:40:34 +0800

    chore: initial trpc

---
+++ src/lib/providers/index.tsx
@@ -0,0 +1,11 @@
+import { TRPCReactProvider } from '@/trpc/client';
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <TRPCReactProvider>
+        {children}
+      </TRPCReactProvider>
+    </>
+  );
+}
